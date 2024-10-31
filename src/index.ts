351b109e5636fff3f5903e7b96f41ebf2bf88fcf
Gabriel Viana <gabrielguilherme13@hotmail.com>
2024-10-31 15:20:35 -0300

    👌 style: change index.ts code lint

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,49 @@
-import { Client, GatewayIntentBits, Events, TextChannel, VoiceChannel } from 'discord.js';
-import { createAudioPlayer, createAudioResource, DiscordGatewayAdapterCreator, getVoiceConnection, joinVoiceChannel, NoSubscriberBehavior, VoiceConnectionStatus } from '@discordjs/voice';
-import { BOT_TOKEN } from './config/config';
+import {
+  Client,
+  GatewayIntentBits,
+  Events,
+  TextChannel,
+  VoiceChannel,
+} from "discord.js";
+import {
+  createAudioPlayer,
+  createAudioResource,
+  DiscordGatewayAdapterCreator,
+  getVoiceConnection,
+  joinVoiceChannel,
+  NoSubscriberBehavior,
+  VoiceConnectionStatus,
+} from "@discordjs/voice";
+import { BOT_TOKEN } from "./config/config";
 
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.GuildVoiceStates
-] });
-
-const cron = require('node-cron');
-const task = cron.schedule('0 0 */1 * * *', async () => {
-    //colocar o dingdong aqui 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
 });
 
-client.once(Events.ClientReady, async client => {
+const cron = require("node-cron");
+const task = cron.schedule("0 0 */1 * * *", async () => {
+  //colocar o dingdong aqui
+});
+
+client.once(Events.ClientReady, async (client) => {
   console.log(`Ready! Logged in as ${client.user?.tag}`);
 });
 
+client.on(Events.ClientReady, async (thisClient) => {
+  const uoltipapo = client.channels.cache.get(
+    "582999750308134916",
+  ) as TextChannel;
 
-client.on(Events.ClientReady, async thisClient => {
-  const uoltipapo = client.channels.cache.get('582999750308134916') as TextChannel;
-  
   //pega o refugo
-  const channel = client.channels.cache.get('1300271303324074005') as VoiceChannel;
+  const channel = client.channels.cache.get(
+    "1300271303324074005",
+  ) as VoiceChannel;
 
-  //tenta conectar 
+  //tenta conectar
   const connection = getVoiceConnection(channel.guild.id);
 
   if (!connection) {
@@ -32,44 +51,48 @@ client.on(Events.ClientReady, async thisClient => {
     const voiceConnection = joinVoiceChannel({
       channelId: channel.id,
       guildId: channel.guild.id,
-      adapterCreator: channel.guild.voiceAdapterCreator as DiscordGatewayAdapterCreator,
+      adapterCreator: channel.guild
+        .voiceAdapterCreator as DiscordGatewayAdapterCreator,
     });
 
     //pega o dingdong nos assest
-    const resource = createAudioResource(__dirname + '/assets/bigben.mp3');
+    const resource = createAudioResource(__dirname + "/assets/bigben.mp3");
 
     //cria o player
-    const player = createAudioPlayer({behaviors: {
-      noSubscriber: NoSubscriberBehavior.Play,
+    const player = createAudioPlayer({
+      behaviors: {
+        noSubscriber: NoSubscriberBehavior.Play,
       },
     });
 
     //testa se a conexao ta pronta
     voiceConnection.on(VoiceConnectionStatus.Ready, () => {
-      console.log('Voice connection is ready!');
+      console.log("Voice connection is ready!");
       //poe o ouvido na boca no player e repete ele
       voiceConnection.subscribe(player);
-      //toca o sino pequenino 
+      //toca o sino pequenino
       player.play(resource);
-
     });
   }
-  uoltipapo?.send('estou cansado chefe')
-})
+  uoltipapo?.send("estou cansado chefe");
+});
 
-client.on(Events.MessageCreate, async message => {
+client.on(Events.MessageCreate, async (message) => {
+  console.log(message)
   if (message.author.bot) return;
-  
-  if (message.content.toLowerCase().includes('civic')) await message.channel.send('Civic?')
-})
 
-client.on(Events.InteractionCreate, async interaction => {
+
+  if (message.content.toLowerCase().includes("civic"))
+    await message.channel.send("Civic?");
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
-  
+
   const { commandName } = interaction;
 
-  if (commandName === 'ping') {
-    await interaction.reply('Pong!');
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
   }
 });
 
