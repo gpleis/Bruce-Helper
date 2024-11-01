b0078ce58728dd2a207e1b0babcae95ad043641d
Gabriel Viana <gabrielguilherme13@hotmail.com>
2024-11-01 01:15:45 -0300

    ♻ refactor: trigger dingdong on messages including 'gongo' and voice mute those traitor whose say they dont like civic

--- src/index.ts
+++ src/index.ts
@@ -51,70 +51,6 @@ client.once(Events.ClientReady, async (client) => {
   const messagePicker = Math.floor(Math.random() * readyMessages.length)
 
   uoltipapo?.send(readyMessages[messagePicker]);
-
-  //pega o refugo
-  // const channel = client.channels.cache.get(
-  //   "1300271303324074005",
-  // ) as VoiceChannel;
-
-  //pega p refugo habitado
-
-  const refugoChannels = client.channels.cache.filter((channel) => {
-    const voiceChannel = channel;
-    if(voiceChannel.type === ChannelType.GuildVoice 
-      && voiceChannel.members.size > 0 
-      && voiceChannel.name.toLowerCase().includes("refugo")) {
-      console.log(`achei a call boa ${voiceChannel.name}`)
-      return voiceChannel;
-    }
-  });
-
-
-  if (refugoChannels.size > 0) {
-    const refugoFinal: VoiceChannel = refugoChannels.sort((a, b) => {
-      const voiceChannelA = a as VoiceChannel;
-      const voiceChannelB = b as VoiceChannel;
-      return voiceChannelB.members.size - voiceChannelA.members.size;
-  
-    }).first() as VoiceChannel;
-    
-    await refugoChannels.forEach((channel) => {
-      if(channel.id === refugoFinal.id) return;
-      const voiceChannel = channel as VoiceChannel;
-      const members = voiceChannel.members;
-      members.forEach((member) => {
-        member.voice.setChannel(refugoFinal);
-      });
-
-    });
-    
-    // aqui ele cria a conexao tarr?
-    const voiceConnection = joinVoiceChannel({
-      channelId: refugoFinal.id,
-      guildId: refugoFinal.guild.id,
-      adapterCreator: refugoFinal.guild
-        .voiceAdapterCreator as DiscordGatewayAdapterCreator,
-    });
-
-    //pega o dingdong nos assest
-    const resource = createAudioResource(__dirname + "/assets/bigben2.mp3");
-
-    //cria o player
-    const player = createAudioPlayer({
-      behaviors: {
-        noSubscriber: NoSubscriberBehavior.Play,
-      },
-    });
-
-    //testa se a conexao ta pronta
-    voiceConnection.on(VoiceConnectionStatus.Ready, () => {
-      console.log("Voice connection is ready!");
-      //poe o ouvido na boca no player e repete ele
-      voiceConnection.subscribe(player);
-      //toca o sino pequenino
-      player.play(resource);
-    });
-  }
 });
 
 client.on(Events.MessageCreate, async (message) => {
@@ -122,14 +58,75 @@ client.on(Events.MessageCreate, async (message) => {
 
   const randInt = Math.floor(Math.random() * 5)
 
-  console.log(message.content, message.content.toLowerCase().includes("civic"));
-
-  if (message.content.toLowerCase().includes("civic"))
+  if (message.content.toLowerCase().includes("nao gosto") && message.content.toLowerCase().includes("civic")) {
+    const member = message.member;
+    member?.voice.setMute(true);
+  }
+  else if (message.content.toLowerCase().includes("civic")) {
     await message.channel.send("Você disse...");
-  
+
     for (let i = 0; i < randInt; i++) {
       await message.channel.send("Civic?");
     }
+  }
+
+  if (message.content === "gongo") {
+    const refugoChannels = client.channels.cache.filter((channel) => {
+      const voiceChannel = channel;
+
+      if (
+          voiceChannel.type === ChannelType.GuildVoice && 
+          voiceChannel.members.size > 0 && 
+          voiceChannel.name.toLowerCase().includes("refugo")
+        ) {
+        return voiceChannel;
+      }
+    });
+
+    if (refugoChannels.size > 0) {
+      const refugoFinal: VoiceChannel = refugoChannels.sort((a, b) => {
+        const voiceChannelA = a as VoiceChannel;
+        const voiceChannelB = b as VoiceChannel;
+        return voiceChannelB.members.size - voiceChannelA.members.size;
+    
+      }).first() as VoiceChannel;
+
+      if (refugoFinal) {
+        await refugoChannels.forEach((channel) => {
+          if(channel.id === refugoFinal.id) return;
+
+          const voiceChannel = channel as VoiceChannel;
+          const members = voiceChannel.members;
+
+          members.forEach((member) => {
+            member.voice.setChannel(refugoFinal);
+          });
+
+        });
+        
+        const voiceConnection = joinVoiceChannel({
+          channelId: refugoFinal.id,
+          guildId: refugoFinal.guild.id,
+          adapterCreator: refugoFinal.guild
+            .voiceAdapterCreator as DiscordGatewayAdapterCreator,
+        });
+
+        const resource = createAudioResource(__dirname + "/assets/bigben2.mp3");
+
+        const player = createAudioPlayer({
+          behaviors: {
+            noSubscriber: NoSubscriberBehavior.Play,
+          },
+        });
+
+        voiceConnection.on(VoiceConnectionStatus.Ready, () => {
+          console.log("Voice connection is ready!");
+          voiceConnection.subscribe(player);
+          player.play(resource);
+        }); 
+      }
+    }
+  }
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
