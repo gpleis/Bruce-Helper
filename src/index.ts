497068701e09aa0033ccdd1d0ce1bec59f0444fa
Gabriel Viana <gabrielguilherme13@hotmail.com>
2024-11-02 12:47:28 -0300

    refactor: change error commands log on loading

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ import registerListeners from './listeners';
     const command = interaction.client.commands.get(interaction.commandName);
 
     if (!command) {
-      console.error(`No command matching ${interaction.commandName} was found.`);
+      console.error(`[ERRO] O comando ${interaction.commandName} não foi encontrado.`);
       return;
     }
 
@@ -49,9 +49,9 @@ import registerListeners from './listeners';
     } catch (error) {
       console.error(error);
       if (interaction.replied || interaction.deferred) {
-        await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        await interaction.followUp({ content: '[ERRO] Ocorreu um erro ao rodar este comando', ephemeral: true });
       } else {
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        await interaction.reply({ content: '[ERRO] Ocorreu um erro ao rodar este comando', ephemeral: true });
       }
     }
   });
