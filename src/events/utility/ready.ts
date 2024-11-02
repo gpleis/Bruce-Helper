9fd08bd9461adaeb808245b769b1df56be5e7986
Gabriel Viana <gabrielguilherme13@hotmail.com>
2024-11-02 17:45:18 -0300

    refactor: change hour format

--- src/events/utility/ready.ts
+++ src/events/utility/ready.ts
@@ -8,5 +8,5 @@ export function execute(client: Client) {
   console.log(`[INFO] Bruce Helper está online! Logado como ${client.user?.tag}`);
 
   const bhDevChannel: TextChannel | undefined = client.channels.cache.get("1302296042196176998") as TextChannel
-  bhDevChannel.send(`Online desde ${format(new Date(), "dd/MM/yyyy - hh:mm:ss")}`)
+  bhDevChannel.send(`Online desde ${format(new Date(), "dd/MM/yyyy - HH:mm:ss")}`)
 }
